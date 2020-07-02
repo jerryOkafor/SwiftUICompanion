7c629d2ebb0b2a37832e639a332d67b2ba55048d
Jerry Hanks <jerryhanksokafor@gmail.com>
2020-07-02 01:27:15 +0100

    Initial Commit

---
+++ SwiftUI Companion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Companion
+//
+//  Created by Jerry Hanks on 02/07/2020.
+//  Copyright © 2020 Jerry Okafor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
